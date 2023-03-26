40c28126b44afa6d0716bd6c1ace309087dfe1b1
lwafulah <loice.lovie@gmail.com>
2023-03-26 10:26:32 -0700

    specifier

--- main.h
+++ main.h
@@ -3,5 +3,28 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int (*check_specifier(const char*))(va_list);
+
+/**
+ * struct funct - struct for printer functions
+ * @t: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions
+ */
+
+typedef struct funct
+{
+	char *t;
+	int (*f)(var_list);
+} funct_t;
+
+
+
+
+
+
+
 
 #endif
