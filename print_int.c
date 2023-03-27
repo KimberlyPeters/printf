ce1eb4e4296a7801f2eddf61a2a91b5295b46159
KimberlyPeters <kimberlypeters468@gmail.com>
2023-03-27 07:46:20 -0700

    Update

--- print_int.c
+++ print_int.c
@@ -10,29 +10,28 @@
 
 int print_int(va_list args)
 {
-	int i = 0;
-	int count = 0;
-	int number;
-	int print_arr[10];
-	char x[1];
+    int i = 0;
+    int count = 0;
+    int number;
+    int print_arr[10];
 
-	number = va_arg(args, int);
+    number = va_arg(args, int);
 
-	while (number != 0)
-	{
-		print_arr[i] = (number % 10);
-		number = number / 10;
-		if (number == 0)
-			break;
-		else
-			i++;
-	}
+    while (number != 0)
+    {
+        print_arr[i] = (number % 10);
+        number = number / 10;
+        if (number == 0)
+            break;
+        else
+            i++;
+    }
 
-	for (i; i >= 0; i--)
-	{
-		x[0] = ('0' + print_arr[i]);
-		count += write(1, x, 1);
-	}
+    for (i; i >= 0; i--)
+    {
+        _putchar('0' + print_arr[i]);
+        count++;
+    }
 
-	return (count);
+    return count;
 }
