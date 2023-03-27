f5fb33d4ca60d51947b308554ff5eda63b99d707
lwafulah <loice.lovie@gmail.com>
2023-03-27 07:17:38 -0700

    conversion specifier c

--- print_char.c
+++ print_char.c
@@ -11,9 +11,9 @@ int print_char(va_list args)
 	char c = (char)va_arg(args, int);
 	int count = 0;
 
-	if (c)
+	if (c != '\0')
 	{
-		count = write(1, &c, 1);
+		_putchar(c);
 		return (count);
 	}
 
