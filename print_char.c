719670b186dc6058a3752b1a27f1e3bf9232a274
root <loice.lovie@gmail.com>
2023-03-28 09:28:03 -0700

    Updated

--- print_char.c
+++ print_char.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 /**
  * print_chr - writes the character c to stdout
- * @args: input char
+ * @arguments: input char
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: On success 1.
  */
-int print_chr(va_list args, char *buf, unsigned int ibuf)
+int print_chr(va_list arguments, char *buf, unsigned int ibuf)
 {
 	char c;
 
-	c = va_arg(args, int);
+	c = va_arg(arguments, int);
 	handl_buf(buf, c, ibuf);
 
 	return (1);
--- print_str.c
+++ print_str.c
@@ -2,18 +2,18 @@
 
 /**
  * print_str - writes the string to stdout
- * @args: input string
+ * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: On success 1.
  */
-int print_str(va_list args, char *buf, unsigned int ibuf)
+int print_str(va_list arguments, char *buf, unsigned int ibuf)
 {
 	char *str;
 	unsigned int i;
 	char nill[] = "(null)";
 
-	str = va_arg(args, char *);
+	str = va_arg(arguments, char *);
 	if (str == NULL)
 	{
 		for (i = 0; nill[i]; i++)
@@ -24,4 +24,3 @@ int print_str(va_list args, char *buf, unsigned int ibuf)
 		ibuf = handl_buf(buf, str[i], ibuf);
 	return (i);
 }
-
