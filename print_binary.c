23b6f7d7e0e10b79caf4ecc2a8e43a742a2b9c1c
root <loice.lovie@gmail.com>
2023-03-28 07:06:36 -0700

    Updated

--- print_binary.c
+++ print_binary.c
@@ -10,29 +10,27 @@
 
 int print_binary(va_list args)
 {
-	int count = 0;
-	unsigned int number;
-	int binary[32];
-	int i;
+	unsigned int number = va_arg(args, unsigned int);
+	int count = 0, i;
+	int binary_digits[100];
 
-	number = va_arg(args, unsigned int);
 	if (number == 0)
 	{
 		_putchar('0');
+		return (1);
+	}
+
+	for (i = 0; number > 0; i++)
+	{
+		binary_digits[i] = number % 2;
+		number = number / 2;
+	}
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar('0' + binary_digits[i]);
 		count++;
 	}
-	else
-	{
-		for (i = 0; number > 0; i++)
-		{
-			binary[i] = number % 2;
-			number /= 2;
-		}
-		for (i = i; i >= 0; i--)
-		{
-			_putchar('0' + binary[i]);
-			count++;
-		}
-	}
+
 	return (count);
 }
