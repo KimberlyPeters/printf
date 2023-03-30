260c0bbc60a3cdbcdc11b7185117edc65617d5d1
root <loice.lovie@gmail.com>
2023-03-30 09:55:25 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -1,9 +1,7 @@
 #include "main.h"
-
 /**
  * _printf - formatted output conversion and print data.
  * @format: input string.
- *
  * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
@@ -13,39 +11,44 @@ int _printf(const char *format, ...)
 	int (*function)(va_list, char *, unsigned int);
 	char *buffer;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+	va_start(arguments, format);
+	if (!format)
 		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+	buffer = malloc(sizeof(char) * 1024);
+	if (!buffer)
+		return (-1);
+	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
+			i++;
+			if (format[i] == '\0')
+			{
+				print_buf(buffer, ibuf);
+				free(buffer);
+				va_end(arguments);
+				return (-1); }
+			function = get_print_func(format[i]);
+			if (function == NULL)
+			{
+				handl_buf(buffer, '%', &ibuf);
+				handl_buf(buffer, format[i], &ibuf);
+				len += 2; }
 			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
+			{
+				len += function(arguments, buffer, &ibuf, format[i + 1]);
+				i++; }
 		}
 		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
-	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
-}
+		{
+			handl_buf(buffer, format[i], &ibuf);
+			len++; }
+		if (ibuf == 1024)
+		{
+			print_buf(buffer, ibuf);
+			ibuf = 0;
+		} i++;
+	} print_buf(buffer, ibuf);
+	free(buffer);
+	va_end(arguments);
+	return (len); }
